d872de630e87bee4134a6939034f54a55bfb7ecc
sharayu0 <sharayurajput1998@gmail.com>
2021-08-27 15:51:09 +0530

    admin deletes user commit

--- admin_dlt_user.php
+++ admin_dlt_user.php
@@ -7,6 +7,8 @@ if(isset($_SESSION['username'])){
   $uid = $_SESSION['uid'];
   $username = $_SESSION['username'];
 
+  echo $uid;
+
 }else{
   header("location:login.php");
 }
@@ -15,10 +17,16 @@ if(isset($_SESSION['username'])){
 
 
 if(isset($_POST['delete_btn'])){
+
+  echo $uid;
+
     $uid=$_POST['delete_id'];
+
+    
+    echo "hello1";
     $sql = "DELETE from users WHERE uid=$uid";
     $result = mysqli_query($conn, $sql);
-    
+
         header("location:admin_index.php?info=deleted");
         exit();
   }
